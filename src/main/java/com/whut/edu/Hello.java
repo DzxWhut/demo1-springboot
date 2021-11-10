c0f25268e0f4a9aa4970b9f37daeb64f190b7fe5
DZX <1621349604@qq.com>
2021-11-10 09:28:42 +0800

    Initial commit

---
+++ src/main/java/com/whut/edu/Hello.java
@@ -0,0 +1,20 @@
+package com.whut.edu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+/**
+ * @className: com.whut.edu.Hello
+ * @description: TODO
+ * @author: DZX
+ * @create: 2021-11-10 8:48
+ */
+@SpringBootApplication
+public class Hello {
+    public static void main(String[] args) {
+        SpringApplication.run(Hello.class,args);
+    }
+
+}
---
+++ src/main/java/com/whut/edu/HelloWorld.java
@@ -0,0 +1,22 @@
+package com.whut.edu;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @className: com.whut.edu.HelloWorld
+ * @description: TODO
+ * @author: DZX
+ * @create: 2021-11-10 8:52
+ */
+@RestController
+public class HelloWorld {
+    @RequestMapping("/")
+    public String sayHello(){
+        return "Hello World!";
+    }
+    @RequestMapping("/int")
+    public int sayInt(){
+        return 1;
+    }
+}
